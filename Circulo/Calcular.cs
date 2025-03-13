a2b048089b076b90fd35fef2147d1170265725bf
Vitoria Ferreira <vitoria.ferrsenac@gmail.com>
2025-03-13 12:00:05 -0300

    feat:mais o meno

---
+++ Circulo/Calcular.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo
+{
+    class Calcular
+    {
+      public  decimal Area { get; set;}
+      public decimal Perimetro { get; set;}
+
+        public Calcular(decimal Area,decimal Perimetro) 
+        {
+            Area = area;
+            Perimetro = perimetro;
+        }
+
+        public void Resultado() 
+        {
+            return $"ÁREA:" { Area}" - " " PERIMETRO " { Perimetro};
+        }
+    }
+}
